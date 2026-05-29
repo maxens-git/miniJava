2490a66f66d48977a4cd8b052c71dc52138279b1
Maxens Verron <maxens.verron+github@outlook.com>
2026-05-29 14:27:14 +0200

    fichiers pour compilation

--- launch_java.sh
+++ launch_java.sh
@@ -1,3 +1,8 @@
 ant -f minijava-build.xml generate
 ant -f minijava-build.xml compile 
-java -cp "bin/cls:tools/commons-lang3-3.7.jar:tools/commons-text-1.2.jar:tools/antlr-4.13.1-complete.jar:$CLASSPATH" fr.n7.stl.minijava.Driver $1
+
+select test in tests_java/test*.mjava; do
+    java -cp "bin/cls:tools/commons-lang3-3.7.jar:tools/commons-text-1.2.jar:tools/antlr-4.13.1-complete.jar:$CLASSPATH" fr.n7.stl.minijava.Driver "$test"
+    java -jar runtam.jar "${test%.mjava}.tam"
+    break
+done
